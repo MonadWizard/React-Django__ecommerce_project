f50dba9ef187eafad5be55b09bb0158243f20ab9
MonadWizard <monad.wizard.r@gmail.com>
2021-05-21 02:43:10 +0600

    add models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,6 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static', )
+]
+
+
+MEDIA_ROOT = 'static/images'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
