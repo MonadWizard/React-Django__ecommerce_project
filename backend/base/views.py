f6ce4015feea6b0198270d3594e60825ebf32258
MonadWizard <monad.wizard.r@gmail.com>
2021-05-24 12:19:14 +0600

    add product serializer

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,11 +1,16 @@
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework import serializers
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 
-from .products import products
+from .models import Product
+from .serializers import ProductSerializer
+
+# from .products import products
 
 # Create your views here.
 @api_view(['GET'])
@@ -30,16 +35,16 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(["GET"])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+
+    return Response(serializer.data)
 
 
 
